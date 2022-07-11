57046ab49dd8b8620591ebcd1d7de3d72ffda9f6
sushant <sgharge299@gmail.com>
2022-07-11 13:09:22 +0530

    [sushant]started teh program with the welcome message.

---
+++ Algorithms.java
@@ -0,0 +1,7 @@
+package DataStructure.AlgorithmPrograms;
+
+public class Algorithms {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Data Structures Algorithm Programs.");
+    }
+}
